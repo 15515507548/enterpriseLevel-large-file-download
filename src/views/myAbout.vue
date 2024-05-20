f66fc144449bf4649b719c01bdc2fd1a843aa1b2
wangjianhua <15515507548@163.com>
2024-05-20 15:24:47 +0800

    提交

--- src/views/myAbout.vue
+++ src/views/myAbout.vue
@@ -30,37 +30,45 @@ export default {
         iconClass: 'el-icon-video-pause', //暂停或上传图标
         controllers: [], //需要取消的接口请求
         terminateRequest: false, //当前的状态是否是暂停
-        allBufferLists: [], //切片下载成功的结果
-        interfaceFailedLists: [] //切片下载接口失败的索引i
+        allBufferLists: [] //切片下载成功的结果
       }
     }
   },
   async created() {
-    //断点续载(类似断点续传)通过localStorage和localforage拿到页面刷新之前保存的最新数据
-    this.file.interfaceFailedLists = localStorage.getItem('interfaceFailedLists')
-      ? JSON.parse(localStorage.getItem('interfaceFailedLists'))
-      : []
+    if (localStorage.getItem('totalChunks')) {
+      this.file.totalChunks = +localStorage.getItem('totalChunks')
+    }
     this.file.isinterfaceFailedfor = localStorage.getItem('isinterfaceFailedfor') === 'true'
     const allBufferLists = await localforage.getItem('allBufferLists')
     if (allBufferLists) {
-      allBufferLists.sort((a, b) => a.idx - b.idx)
       this.file.allBufferLists = allBufferLists
       if (allBufferLists.length >= 1) {
         //通过allBufferLists从小到大排序后获取最后一条数据拿到当前开始下载的loadSize和percentage
-        const lastObj = allBufferLists[allBufferLists.length - 1]
-        this.file.loadSize = lastObj.loadSize
-        this.file.percentage = lastObj.percentage
+        const loadSizes = allBufferLists.map((item) => item.loadSize)
+        const percentages = allBufferLists.map((item) => item.percentage)
+        this.file.loadSize = Math.max(...loadSizes)
+        this.file.percentage = Math.max(...percentages)
       }
     }
     console.log(
       this.file.allBufferLists,
       this.file.loadSize,
+      this.file.totalChunks,
       this.file.percentage,
-      this.file.interfaceFailedLists,
       'created'
     )
   },
   methods: {
+    pauseAndContinueDownload(file) {
+      if (file.isinterfaceFailedfor) {
+        this.lastFn(file)
+      } else {
+        const obj = file.allBufferLists.find((item) => item.loadSize === file.loadSize)
+        if (obj) {
+          this.continueUpload(file, obj.idx + 1)
+        }
+      }
+    },
     async handlePausePlay(file) {
       if (file.iconClass == 'el-icon-video-pause') {
         file.iconClass = 'el-icon-video-play'
@@ -73,14 +81,7 @@ export default {
       } else {
         file.terminateRequest = false
         file.iconClass = 'el-icon-video-pause'
-        if (file.isinterfaceFailedfor) {
-          this.lastFn(file)
-        } else {
-          const obj = file.allBufferLists.find((item) => item.loadSize === file.loadSize)
-          if (obj) {
-            this.continueUpload(file, obj.idx + 1)
-          }
-        }
+        this.pauseAndContinueDownload(file)
       }
     },
     async forFn(i, file) {
@@ -113,7 +114,6 @@ export default {
             .then((res) => {
               if (+res.status === 206) {
                 file.lists.delete(promise)
-                file.interfaceFailedLists = file.interfaceFailedLists.filter((nub) => nub != i)
                 file.loadSize++
                 file.percentage = +((file.loadSize / file.totalChunks) * 100).toFixed(0)
                 file.allBufferLists.push({
@@ -122,23 +122,14 @@ export default {
                   loadSize: file.loadSize,
                   percentage: file.percentage
                 })
-                localStorage.setItem(
-                  'interfaceFailedLists',
-                  JSON.stringify(file.interfaceFailedLists)
-                )
+                console.log('then')
                 return
               }
               throw '下载失败，请您稍后再试~~'
             })
             .catch((err) => {
+              console.log(err, 66666666)
               file.lists.delete(promise)
-              if (!file.interfaceFailedLists.includes(i)) {
-                file.interfaceFailedLists.push(i)
-                localStorage.setItem(
-                  'interfaceFailedLists',
-                  JSON.stringify(file.interfaceFailedLists)
-                )
-              }
               index++
               sgfd(i, fn, index)
             })
@@ -179,10 +170,29 @@ export default {
       await Promise.allSettled(file.lists)
       await this.lastFn(file)
     },
+    bufferMerge(file) {
+      file.allBufferLists.sort((a, b) => a.idx - b.idx)
+      const arrBufferList = file.allBufferLists.map((item) => new Uint8Array(item.buffer))
+      const allBuffer = this.concatenate(Uint8Array, arrBufferList)
+      const blob = new Blob([allBuffer])
+      let blobURL = window.URL.createObjectURL(blob)
+      const elink = document.createElement('a')
+      elink.download = file.obj.filename
+      elink.href = blobURL
+      elink.target = '_blank'
+      elink.style.display = 'none'
+      document.body.appendChild(elink)
+      elink.click()
+      window.URL.revokeObjectURL(blobURL)
+      document.body.removeChild(elink)
+    },
     //所有请求完成后，失败请求接口重试
     async lastFn(file) {
-      if (file.interfaceFailedLists.length) {
-        for (const i of file.interfaceFailedLists) {
+      if (file.allBufferLists.length == file.totalChunks) {
+        this.bufferMerge(file)
+      } else {
+        console.log(456321)
+        for (let i = 0; i < file.totalChunks; i++) {
           const obj = file.allBufferLists.find((item) => item.idx === i)
           if (obj) {
             continue
@@ -197,37 +207,23 @@ export default {
           }
           await this.forFn(i, file)
         }
+        await Promise.allSettled(file.lists)
+        if (file.allBufferLists.length !== file.totalChunks) {
+          this.$message.error('下载失败，请您稍后再试~~')
+        } else {
+          this.bufferMerge(file)
+        }
       }
-      await Promise.allSettled(file.lists)
       console.log(file.allBufferLists, file.loadSize, file.totalChunks, file.percentage, '接口完毕')
-      // if (file.iconClass == 'el-icon-video-play') return
-      if (file.loadSize != file.totalChunks) {
-        this.$message.error('下载失败，请您稍后再试~~')
-      } else {
-        file.allBufferLists.sort((a, b) => a.idx - b.idx)
-        const arrBufferList = file.allBufferLists.map((item) => new Uint8Array(item.buffer))
-        const allBuffer = this.concatenate(Uint8Array, arrBufferList)
-        const blob = new Blob([allBuffer])
-        let blobURL = window.URL.createObjectURL(blob)
-        const elink = document.createElement('a')
-        elink.download = file.obj.filename
-        elink.href = blobURL
-        elink.target = '_blank'
-        elink.style.display = 'none'
-        document.body.appendChild(elink)
-        elink.click()
-        window.URL.revokeObjectURL(blobURL)
-        document.body.removeChild(elink)
-      }
       file.isShow = false
       file.loadSize = 0
       file.percentage = 0
       file.controllers = []
       file.isinterfaceFailedfor = false
       localStorage.removeItem('isinterfaceFailedfor')
+      file.totalChunks = 0
+      localStorage.removeItem('totalChunks')
       file.allBufferLists = []
-      file.interfaceFailedLists = []
-      localStorage.removeItem('interfaceFailedLists')
       await localforage.clear()
     },
     concatenate(resultConstructor, arrays) {
@@ -291,24 +287,9 @@ export default {
           file.lists = new Set()
           file.controllers = []
           file.totalChunks = Math.ceil(file.sizeLength / file.chunkSize)
-
+          localStorage.setItem('totalChunks', file.totalChunks)
           if (file.percentage) {
-            if (file.isinterfaceFailedfor) {
-              this.lastFn(file)
-            } else {
-              const obj = file.allBufferLists.find((item) => item.loadSize === file.loadSize)
-              console.log(
-                file.allBufferLists,
-                file.loadSize,
-                file.totalChunks,
-                file.percentage,
-                obj,
-                '成功'
-              )
-              if (obj) {
-                this.continueUpload(file, obj.idx + 1)
-              }
-            }
+            this.pauseAndContinueDownload(file)
           } else {
             this.continueUpload(file, 0)
           }
